cbdca601832bcee50a4e924891939088d30f23ae
Vikaram <vikramstuart23@gmail.com>
2022-11-01 11:34:34 +0530

    01Nov2022 update 2 (11:34)

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -1,81 +1,70 @@
 public class ChessBoard {
-    static double moves=0;
+    static double moves = 0;
+
     public static void main(String[] args) {
-        int n = 3, k = 2, row = 0, col = 0,steps =1;
-//        int[][] array = new int[n][n];
-        recursion(row,col,n,k,steps);
+        int n = 3, k = 3, row = 0, col = 0, steps = 1;
+        recursion(row, col, n, k, steps);
         System.out.println(moves);
     }
-    public static void recursion(int row, int col,int n,int k,int steps){
-        if(row+2>=0 && col-1>=0 && row+2<n && col-1<n) {
-            if (steps == k){
+
+    public static void recursion(int row, int col, int n, int k, int steps) {
+        if (row + 2 < n && col - 1 >= 0) {
+            if (steps == k) {
                 moves += 1 / Math.pow(8, steps);
             } else {
-                recursion(row,col,n,k,steps+1);
+                recursion(row+2, col-1, n, k, steps + 1);
             }
         }
-        if(row+2>=0 && col+1 >=0 && row+2<n && col+1<n) {
-            if(row+2>=0 && col-1>=0 && row+2<n && col-1<n) {
-                if (steps == k) {
-                    moves += 1 / Math.pow(8, steps);
-                } else {
-                    recursion(row, col, n, k, steps + 1);
-                }
+        if (row + 2 < n && col + 1 < n) {
+            if (steps == k) {
+                moves += 1 / Math.pow(8, steps);
+            } else {
+                recursion(row+2, col+1, n, k, steps + 1);
             }
-        if(row-2>=0 && col-1>=0 && row-2<n && col-1<n)
-        {
-            if(row+2>=0 && col-1>=0 && row+2<n && col-1<n) {
-                if (steps == k) {
-                    moves += 1 / Math.pow(8, steps);
-                } else {
-                    recursion(row, col, n, k, steps + 1);
-                }
+        }
+        if (row - 2 >=0 && col - 1 >= 0) {
+            if (steps == k) {
+                moves += 1 / Math.pow(8, steps);
+            } else {
+                recursion(row-2, col-1, n, k, steps + 1);
             }
-        if(row-2>=0 && col+1 >=0 && row-2<n && col+1<n)
-        {
-            if(row+2>=0 && col-1>=0 && row+2<n && col-1<n) {
-                if (steps == k) {
-                    moves += 1 / Math.pow(8, steps);
-                } else {
-                    recursion(row, col, n, k, steps + 1);
-                }
+        }
+        if (row - 2 >=0 && col + 1 <n) {
+            if (steps == k) {
+                moves += 1 / Math.pow(8, steps);
+            } else {
+                recursion(row-2, col+1, n, k, steps + 1);
             }
-        if(col+2>=0 && row-1>=0 && col+2<n && row-1<n)
-        {
-            if(row+2>=0 && col-1>=0 && row+2<n && col-1<n) {
-                if (steps == k) {
-                    moves += 1 / Math.pow(8, steps);
-                } else {
-                    recursion(row, col, n, k, steps + 1);
-                }
+        }
+        if (col + 2 < n && row - 1 >= 0) {
+            if (steps == k) {
+                moves += 1 / Math.pow(8, steps);
+            } else {
+                recursion(row-1, col+2, n, k, steps + 1);
             }
-        if(col+2>=0 && row+1>=0 && col+2<n && row+1<n)
-        {
-            if(row+2>=0 && col-1>=0 && row+2<n && col-1<n) {
-                if (steps == k) {
-                    moves += 1 / Math.pow(8, steps);
-                } else {
-                    recursion(row, col, n, k, steps + 1);
-                }
+        }
+        if (col + 2 < n && row + 1 < n) {
+            if (steps == k) {
+                moves += 1 / Math.pow(8, steps);
+            } else {
+                recursion(row+1, col+2, n, k, steps + 1);
             }
-        if(col-2>=0 && row-1>=0 && col-2<n && row-1<n)
-        {
-            if(row+2>=0 && col-1>=0 && row+2<n && col-1<n) {
-                if (steps == k) {
-                    moves += 1 / Math.pow(8, steps);
-                } else {
-                    recursion(row, col, n, k, steps + 1);
-                }
+        }
+
+        if (col - 2 >= 0 && row + 1 < n) {
+            if (steps == k) {
+                moves += 1 / Math.pow(8, steps);
+            } else {
+                recursion(row+1, col-2, n, k, steps + 1);
             }
-        if(col-2>=0 && row+1>=0 && col-2<n && row+1<n) {
-            if (row + 2 >= 0 && col - 1 >= 0 && row + 2 < n && col - 1 < n) {
-                if (steps == k) {
-                    moves += 1 / Math.pow(8, steps);
-                } else {
-                    recursion(row, col, n, k, steps + 1);
-                }
+        }
+
+        if (col - 2 >= 0 && row - 1 >= 0) {
+            if (steps == k) {
+                moves += 1 / Math.pow(8, steps);
+            } else {
+                recursion(row-1, col-2, n, k, steps + 1);
             }
         }
     }
-
-}}}}}}}
+}
